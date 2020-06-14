be6ad81deafb453f10ff9eb0027f0a587941e282
jmb1350 <65556581+jmb1350@users.noreply.github.com>
2020-06-14 15:15:46 -0400

    Lastest Price & Last Refresh date fed in

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,20 +3,27 @@
 import requests
 import json
 
+
+def to_usd(my_price):
+    return f"${my_price:,.2f}"
+
+
+
 #Info Inputs
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
 requests.get(request_url)
 
 response = requests.get(request_url)
-print(type(response))   #requets.models.Response (data type)
-print(response.status_code)  #200
-print(response.text)        #string verison of dictionary object
+#print(type(response))   #requets.models.Response (data type)
+#print(response.status_code)  #200
+#print(response.text)        #string verison of dictionary object
 
 parsed_response = json.loads(response.text)
 
-last_refreshed = parsed_response["Meta Deta"]["3. Last Refreshed"]
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+latest_close = parsed_response["Time Series (Daily)"]["2020-06-12"]["4. close"]
 
 
 #breakpoint()
@@ -24,13 +31,13 @@ last_refreshed = parsed_response["Meta Deta"]["3. Last Refreshed"]
 #Info Outputs
 
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL: IBM")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")

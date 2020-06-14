bb194c91b58201f287f67d93944efe37831c89b5
jmb1350 <65556581+jmb1350@users.noreply.github.com>
2020-06-14 10:35:01 -0400

    Imported JSON and Parsed info into Dictionary

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,25 @@
 # app/robo_advisor.py
 
+import requests
+import json
+
+#Info Inputs
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+requests.get(request_url)
+
+response = requests.get(request_url)
+print(type(response))   #requets.models.Response (data type)
+print(response.status_code)  #200
+print(response.text)        #string verison of dictionary object
+
+parsed_response = json.loads(response.text)
+
+
+breakpoint()
+
+#Info Outputs
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")

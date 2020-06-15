fead0091de2b72c451006e64a532cd626784af08
jmb1350 <65556581+jmb1350@users.noreply.github.com>
2020-06-15 13:04:10 -0400

    Formula Written - Stock information returned. Error tickers filtered out and returns "try again" message

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,8 +3,12 @@
 import json
 import csv
 import os
+import datetime
 
+from dotenv import load_dotenv
 import requests
+
+load_dotenv()
 
 
 def to_usd(my_price):
@@ -12,37 +16,59 @@ def to_usd(my_price):
 
 
 
-#Info Inputs
+#Info Inputs and Data Requests
+api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+symbol = input("Please input a ticker symbol:")
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+
+while symbol.isdigit():
+    symbol = input("Sorry, I couldn't understand that symbol. Please try again: ")
+   #https://www.w3schools.com/python/ref_string_isdigit.asp
+   #filtered out digits from ticker without len function, since tickers can be a single letter
+
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+
 requests.get(request_url)
 
 response = requests.get(request_url)
-#print(type(response))   #requets.models.Response (data type)
-#print(response.status_code)  #200
-#print(response.text)        #string verison of dictionary object
 
-parsed_response = json.loads(response.text)
+parsed_response = json.loads(response.text) 
+
+if "Error" in response.text:   
+        print("Sorry, data isn't available. Please try again later.") 
+        exit()
 
 
-#dates
-dtime = parsed_response["Time Series (Daily)"]
-dates = list(dtime.keys())
 
-latest_day = dates[0]  #sort to ensure latest day is first - right now assumes 1st day
+
+
+
+
+#DATES
+
+tsd = parsed_response["Time Series (Daily)"]
+
+dates = list(tsd.keys())
+
+latest_day = dates[0]  
+
+latest_close = tsd[latest_day]["4. close"]
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-latest_close = dtime[latest_day]["4. close"]
 
 
-#pricing data
+
+
+
+#PRICING DATA
 high_prices = []
 low_prices = []
 
 for date in dates:
-    high_price = dtime[date]["2. high"]
-    low_price = dtime[date]["3. low"]
+    high_price = tsd[date]["2. high"]
+    low_price = tsd[date]["3. low"]
     high_prices.append(float(high_price))
     low_prices.append(float(low_price))
 
@@ -52,7 +78,14 @@ recent_low = min(low_prices)
 
 
 
-#write to CSV
+
+
+
+
+
+
+
+#WRITING TO CSV
 
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
 
@@ -61,7 +94,7 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
     writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
     writer.writeheader() # uses fieldnames set above 
     for date in dates:
-        daily_prices = dtime[date]
+        daily_prices = tsd[date]
         writer.writerow({
             "timestamp": date,
             "open": daily_prices["1. open"],
@@ -76,10 +109,15 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
 
 
 
-#Info Outputs
+
+
+
+
+
+#INFO OUTPUTS
 
 print("-------------------------")
-print("SELECTED SYMBOL: IBM")
+print("SELECTED SYMBOL: " + symbol)
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
@@ -88,9 +126,19 @@ print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print(f"RECENT LOW: {to_usd(float(recent_low))}")
+
+high_multiple = float(recent_high) * .2
+close_formula = float(high_multiple) + float(latest_close)
+
+#https://www.journaldev.com/23715/python-convert-string-to-float#:~:text=We%20can%20convert%20a%20string,object%20__float__()%20function.
+#used the above webite to realize that everything had to be converted to float for the calculations
+
 print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: Good Investment")
+
+if float(close_formula) <= float(recent_high):
+    print("RECOMMENDATION: Buy! This stock is a good deal!")
+else:
+    print("RECOMMENDATION: Don't buy! This stock is too expensive.")
 print("-------------------------")
 print(f"WRITING DATA TO CSV: {csv_file_path}")
 print("-------------------------")
@@ -98,3 +146,7 @@ print("HAPPY INVESTING!")
 print("-------------------------")
 
 
+
+
+
+#print("Sorry, data isn't available. Please try again later.")    
